3eecf049bb46697de7154b2fa6a0a3ca212e6fe3
Dan Souza <dan.souza@tamr.com>
2020-06-22 13:52:49 -0400

    Cleanup fomratting

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,14 +1,14 @@
 module "rds_postgres" {
-  source = "git::https://github.com/Datatamer/terraform-rds-postgres.git?ref=0.1.0"
-  postgres_name = "example_rds_postgres"
+  source               = "git::https://github.com/Datatamer/terraform-rds-postgres.git?ref=0.1.0"
+  postgres_name        = "example_rds_postgres"
   parameter_group_name = "example-rds-postgres-pg"
-  identifier_prefix = "example-rds-"
+  identifier_prefix    = "example-rds-"
 
   username = "exampleUsername"
   password = "examplePassword"
 
-  subnet_name = "example_subnet"
+  subnet_name          = "example_subnet"
   spark_cluster_sg_ids = ["sg-examplesecuritygroup1", "sg-examplesecuritygroup2"]
-  tamr_vm_sg_id = "sg-exampletamrsecuritygroup"
-  vpc_id = "vpc-examplevpcnetworkid"
+  tamr_vm_sg_id        = "sg-exampletamrsecuritygroup"
+  vpc_id               = "vpc-examplevpcnetworkid"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "rds_postgres_pg_id" {
-  value = aws_db_parameter_group.rds_postgres_pg.id
+  value       = aws_db_parameter_group.rds_postgres_pg.id
   description = "ID of the RDS postgres parameter group"
 }
 
 output "rds_postgres_id" {
-  value = aws_db_instance.rds_postgres.id
+  value       = aws_db_instance.rds_postgres.id
   description = "ID of the of the RDS instance"
 }
 
 output "rds_sg_id" {
-  value = module.rds_sg.rds_sg_id
+  value       = module.rds_sg.rds_sg_id
   description = "ID of the security group attached to the rds instance"
 }
 
