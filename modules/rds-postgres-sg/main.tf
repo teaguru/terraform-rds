6db2bf955eb9bea7cd0c294ddabb62451d1ed369
daniosim <dani.sim@tamr.com>
2020-10-12 14:27:07 -0400

    DEV-14278 Changes to fix AWS018 - Add descriptions for sg rules.

--- modules/rds-postgres-sg/main.tf
+++ modules/rds-postgres-sg/main.tf
@@ -6,6 +6,7 @@ resource "aws_security_group" "rds_postgres_sg" {
 }
 
 resource "aws_security_group_rule" "tamr_vm" {
+  description              = "Rule for ingress from Tamr VM to Postgres"
   from_port                = 5432
   protocol                 = "tcp"
   security_group_id        = aws_security_group.rds_postgres_sg.id
@@ -15,6 +16,7 @@ resource "aws_security_group_rule" "tamr_vm" {
 }
 
 resource "aws_security_group_rule" "spark_cluster" {
+  description              = "Rule for ingress from Spark cluster to Postgres"
   count                    = length(var.spark_cluster_sg_ids)
   from_port                = 5432
   protocol                 = "tcp"
@@ -25,6 +27,7 @@ resource "aws_security_group_rule" "spark_cluster" {
 }
 
 resource "aws_security_group_rule" "additional_cidrs" {
+  description       = "Rule for ingress from additional CIDRs to Postgres"
   count             = length(var.additional_cidrs) == 0 ? 0 : 1
   from_port         = 5432
   protocol          = "tcp"
